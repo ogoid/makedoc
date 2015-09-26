c8a0949a40c12d612cd2864cd0b32b858a40c537
Diogo Ramos <somar.ogoid@hotmail.com>
2015-09-26 00:59:07 -0300

    runner rewrite

--- src/Runner.hs
+++ src/Runner.hs
@@ -7,64 +7,38 @@ import Data.List           (isPrefixOf, nub)
 import Control.Monad       (void, when)
 import System.FilePath     (splitExtension)
 import System.Directory    (findExecutable)
-import System.Process      (readProcess)
+import System.Process      (readProcess, callProcess)
 
 
-data Command = None | Biber | Bibtex | LaTeX deriving (Eq)
+data Command = None | LaTeX | LaTeXMk | Biber | Bibtex deriving (Eq, Show)
 
--- runs latex and biber/bibtex until done
-runLaTeX :: String -> FilePath -> IO ()
-runLaTeX program file = maybe withoutMk withMk =<< findExecutable "latexmk"
+runLaTeX :: String -> String -> IO ()
+runLaTeX program file = go 0 . choose =<< findExecutable "latexmk"
   where
-  withMk = const $ execLatexMk program base
-  base = case splitExtension file of
-          (b, ".tex") -> b
-          (b, _) -> file
-  withoutMk = let go _ None = return ()
-                  go 0 _ = error "too many tex executions"
-                  go n Biber  = execBiber base >> go (n-1) LaTeX
-                  go n Bibtex = execBibtex base >> go (n-1) LaTeX
-                  go n LaTeX  = execTex program base >>= go (n-1)
-              in go 5 LaTeX
+    choose = maybe LaTeX (const LaTeXMk)
+    go _ None = return ()
+    go 5 _    = error "too many tex executions"
+    go n cmd  = do
+      let base = case splitExtension file of
+                 (b, ".tex") -> b
+                 (b, _) -> file
+      putStrLn $ "Running: " ++ show cmd ++ " " ++ base
+      run cmd program base >>= go (n+1)
 
-execLatexMk :: String -> FilePath -> IO ()
-execLatexMk program file = do
-  putStrLn ("Running: latexmk " ++ file)
-  void $ readProcess "latexmk"
-          [ "-pdflatex=" ++ program
-          , "-quiet", "-pdf"
-          , file ] ""
-
-execBiber :: FilePath -> IO ()
-execBiber file = do
-  putStrLn ("Running: biber " ++ file)
-  void $ readProcess "biber" ["-q", "-q", file] ""
-
-execBibtex :: FilePath -> IO ()
-execBibtex file = do
-  putStrLn ("Running: bibtex " ++ file)
-  void $ readProcess "bibtex" [file] ""
-
-
--- runs latex once
-execTex :: String -> FilePath -> IO Command
-execTex program file = do
-  putStrLn $ "Running: " ++ program ++ " " ++ file
-  res <- readProcess program
-          ["-halt-on-error",
-          "-interaction",
-          "nonstopmode",
-          file]
-          ""
-  return $ chooseCmd $ nub $ map toCommand $ lines res
+run :: Command -> String -> String -> IO Command
+run Biber   _       base = callProcess "biber" ["-q", "-q", base] >> return LaTeX
+run Bibtex  _       base = callProcess "bibtex" [base] >> return LaTeX
+run LaTeXMk program base = callProcess "latexmk" opts >> return None
+              where opts = [ "-pdflatex=" ++ program, "-quiet", "-pdf", base ]
+run LaTeX   program base = processLog <$> readProcess program opts ""
   where
-  chooseCmd :: [Command] -> Command
-  chooseCmd cmds
+  opts = [ "-halt-on-error", "-interaction", "nonstopmode", base ]
+  processLog = choose . nub . map toCommand . lines
+  choose cmds
     | Biber  `elem` cmds = Biber
     | Bibtex `elem` cmds = Bibtex
     | LaTeX  `elem` cmds = LaTeX
     | otherwise = None
-  toCommand :: String -> Command
   toCommand line
     | "Package biblatex Warning: Please (re)run Biber" `isPrefixOf` line = Biber
     | "Package biblatex Warning: Please rerun LaTeX" `isPrefixOf` line = LaTeX
